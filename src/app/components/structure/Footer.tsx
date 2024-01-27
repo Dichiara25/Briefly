6de65afc97142a1fb0b6d8b8f207175598874f37
Dichiara25 <edclrcq@gmail.com>
2024-01-27 16:00:33 +0100

    Lighten footer

--- src/app/components/structure/Footer.tsx
+++ src/app/components/structure/Footer.tsx
@@ -11,18 +11,22 @@ export default function Footer() {
 
     return <div className={styles.container} style={{flexDirection: "column", textAlign: "center"}}>
         <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-            <div style={{display: "flex"}}>
-                <div>🗞️</div>
-                <h1 style={{fontSize: 'x-large', margin: '0 5px'}}>Briefly</h1>
+            <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <div style={{display: "flex", flexDirection: "row"}}>
+                    <div>🗞️</div>
+                    <h1 style={{fontSize: 'x-large', margin: '0 5px'}}>Briefly</h1>
+                </div>
+                <div style={{opacity: ".4", marginTop: "20px", fontSize: "large"}}>
+                    © {APP_NAME} {currentYear}
+                </div>
             </div>
             <div style={{margin: "0 50px", opacity: ".3"}}>
                 |
             </div>
-            <div style={{display: "flex", flexDirection: "column", opacity: ".7"}}>
+            <div style={{display: "flex", flexDirection: "column", opacity: ".4"}}>
                 <a style={{margin: "10px 0"}} href={routes.privacy}>Privacy</a>
                 <a style={{margin: "10px 0"}} href={routes.termsOfUse}>Terms of use</a>
             </div>
         </div>
-        <p>Made with ❤️ in Paris | © {APP_NAME} {currentYear}</p>
     </div>
 }
